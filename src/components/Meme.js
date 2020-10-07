e294bd65cb5e9ff8cce9cae7110eccf59b79f1fa
Shubham Jha <shubham.jha2001m@gmail.com>
2020-10-07 17:55:52 +0700

    Components

---
+++ src/components/Meme.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+class Meme extends Component {
+
+    render(){
+        return (
+            <div>
+                <h1>Memes</h1>
+            </div>
+            )
+    }
+}
+
+export default Meme;
